f0f90ac02e23f22d70d1d0eeb19cdb51a90503bd
Gautzelin <joselyncorrea600@gmail.com>
2024-11-21 20:22:49 -0500

    Reestructurar la lógica de registro de usuarios

--- src/main/java/proyecto/gestionBiblioteca/configuration/SecurityConfiguration.java
+++ src/main/java/proyecto/gestionBiblioteca/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                        .requestMatchers("/", "/nuevoUsuario","/guardarUsuario", "/libro/lista", "/assets/**", "/40*")
+                        .requestMatchers("/nuevoUsuario","/guardarUsuario", "/libro/lista", "/assets/**")
                         .permitAll()
                         .requestMatchers("/*","/**")
                         .hasAuthority("ADMINISTRADOR")
